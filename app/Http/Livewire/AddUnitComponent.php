093cf035eb781f3b5c96a0fa5347d4cdc0a97e86
islam kabbary <islamkabbarygmailcom>
2021-04-09 05:22:39 +0200

    order_product

--- app/Http/Livewire/AddUnitComponent.php
+++ app/Http/Livewire/AddUnitComponent.php
@@ -12,9 +12,9 @@ class AddUnitComponent extends Component
     public $unit_id;
 
     protected $rules = [
-       'name' => 'required',
+        'name' => 'required',
 
-   ];
+    ];
 
     public function render()
     {
@@ -25,36 +25,38 @@ class AddUnitComponent extends Component
         $this->validateOnly($propertyName);
     }
 
-    public function save(){
+    public function save()
+    {
         $this->validate();
 
 
-    if($this->unit_id)
-        $unit = Unit::find($this->unit_id);
-    else {
-        $unit = new Unit();
-    }
+        if ($this->unit_id){
+            $unit = Unit::find($this->unit_id);
+        }
+
+        else {
+            $unit = new Unit();
+        }
 
         $unit->name = $this->name;
 
         $unit->save();
         $this->clear();
-
-
     }
-    function delete($unit_id){
+    function delete($unit_id)
+    {
         Unit::destroy($unit_id);
     }
 
-    function edit($unit_id){
-        $unit =Unit::find($unit_id);
+    function edit($unit_id)
+    {
+        $unit = Unit::find($unit_id);
         $this->unit_id = $unit->id;
         $this->name = $unit->name;
-
     }
-    function clear(){
+    function clear()
+    {
         $this->unit_id = null;
         $this->name = null;
-
     }
 }
--- app/Http/Livewire/CategoryComponent.php
+++ app/Http/Livewire/CategoryComponent.php
@@ -8,7 +8,7 @@ use Livewire\Component;
 class CategoryComponent extends Component
 {
     public $sub_id;
-    public $name ;
+    public $name;
     public $for;
     public $category_id;
 
@@ -28,33 +28,33 @@ class CategoryComponent extends Component
         $this->validateOnly($propertyName);
     }
 
-    public function save(){
+    public function save()
+    {
 
         $this->validate();
 
-$this->validate();
-if($this->sub_id){
-    $category= Category::find($this->sub_id);
-}
-else{
-$category = new category();
-}
-$category->name=$this->name;
-$category->category_id = $this->category_id;
-$category->save();
-}
+        $this->validate();
+        if ($this->sub_id) {
+            $category = Category::find($this->sub_id);
+        } else {
+            $category = new category();
+        }
+        $category->name = $this->name;
+        $category->category_id = $this->category_id;
+        $category->save();
+    }
 
 
-function delete($sub_id){
-    Category::destroy($sub_id);
-}
+    function delete($sub_id)
+    {
+        Category::destroy($sub_id);
+    }
 
-function edit($sub_id){
-    $category =Category::find($sub_id);
-    $this->for=$category->id;
-    $this->category_id = $category->category_id;
-    $this->name = $category->name;
- }
-
-
+    function edit($sub_id)
+    {
+        $category = Category::find($sub_id);
+        $this->for = $category->id;
+        $this->category_id = $category->category_id;
+        $this->name = $category->name;
+    }
 }
